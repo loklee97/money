fb05d49a06c947b68c45d6ce0dbbc7c95d327f25
loklee97 <37098654+loklee97@users.noreply.github.com>
2025-06-14 16:29:06 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+    server: {
+    proxy: {
+      '/api': {
+        target: 'https://moneyapi-lj06.onrender.com/', // Render API 地址
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      }
+    }
+  }
 })
+

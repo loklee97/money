9b8c36f83b7442c39db84e15a56968f904fd8c5b
loklee97 <loklee97@gmail.com>
2025-07-03 16:50:16 +0800

    fixed some bugs and learning git practice

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,21 +3,21 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 import { useState } from 'react';
 import logoImage from '../assets/download.png';
-import { recalculateapi, returnLogin} from '../api/RecordAPI.ts'
+import { recalculateapi, returnLogin } from '../api/RecordAPI.ts'
 export default function Header({ }) {
-    const { money, user, logout,resetMoney } = useAuth();
+    const { money, user, logout, resetMoney } = useAuth();
     const [showMoney, setShowMoney] = useState(true);
     const navigate = useNavigate();
-    const handleRecalculate =async () =>{
-       const newMoney =  await recalculateapi(user!)
-       console.log('new money : ',newMoney)
-        resetMoney( newMoney)
+    const handleRecalculate = async () => {
+        const newMoney = await recalculateapi(user!)
+        console.log('new money : ', newMoney)
+        resetMoney(newMoney)
     }
-    
-    
+
+
     return (
         <header className="flex items-center justify-between px-6 py-3 bg-gray-800 text-white shadow-md">
-            <div className="flex items-center gap-2  hover:opacity-80 " onClick={() => navigate('/home')}>
+           <div className="flex items-center gap-2  hover:opacity-80 " onClick={() => navigate('/home')}>
                 <img
                     src={logoImage}
                     alt="Logo"
@@ -27,7 +27,7 @@ export default function Header({ }) {
             </div>
 
             <div className="flex items-center gap-4">
-                   <button onClick={() => handleRecalculate()} className="text-sm cursor-pointer">
+                <button onClick={() => handleRecalculate()} className="text-sm cursor-pointer">
                     Recalculate
                 </button>
                 <button onClick={() => setShowMoney(!showMoney)} className="text-sm cursor-pointer">
